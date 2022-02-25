eaeaf08ae243ad5aed319c7865cc879297464e13
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-02-24 21:06:05 -0700

    Refactoring LDtkMapRenderer to be less yucky

--- Raspberry/Raspberry_Lib/Renderers/LDtkMapRenderer.cs
+++ Raspberry/Raspberry_Lib/Renderers/LDtkMapRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using LDtk;
@@ -11,15 +12,21 @@ namespace Raspberry_Lib.Renderers
 {
     internal class LDtkMapRenderer : RenderableComponent
     {
-        public LDtkMapRenderer(string iPath, ContentManager iContentManager)
+        public LDtkMapRenderer(string iPath, ContentManager iContentManager, int iLevelIndex = 0)
         {
             _path = iPath;
             _contentManager = iContentManager;
             _world = LDtkWorld.LoadWorld(Path.Combine(_contentManager.RootDirectory, _path));
+            _level = iLevelIndex;
+
+            System.Diagnostics.Debug.Assert(_world != null);
+            System.Diagnostics.Debug.Assert(0 <= _level && _level < _world.Levels.Length);
+            System.Diagnostics.Debug.Assert(_contentManager != null);
+            _layerToTileMap = PreloadLevelTilesets();
         }
 
-        public override float Width => _world.Levels[0].PxWid;
-        public override float Height => _world.Levels[0].PxHei;
+        public override float Width => _world.Levels[_level].PxWid;
+        public override float Height => _world.Levels[_level].PxHei;
 
         // ReSharper disable once IdentifierTypo
         public override void Render(Batcher iBatcher, Camera iCamera)
@@ -27,53 +34,21 @@ namespace Raspberry_Lib.Renderers
             var level = _world.Levels[0];
             System.Diagnostics.Debug.Assert(Math.Abs(Entity.Transform.Scale.X - Entity.Transform.Scale.Y) < .001);
             var scale = Entity.Transform.Scale.X;
-            for (var index = level.LayerInstances.Length - 1; index >= 0; --index)
+            for (var ii = level.LayerInstances.Length - 1; ii >= 0; --ii)
             {
-                var layer = level.LayerInstances[index];
-                if (layer._TilesetRelPath != null && layer._Type != LayerType.Entities)
-                {
-                    var texturePath = Path.Combine(GetParentPath(_path), layer._TilesetRelPath);
-                    var texture = this.LoadTexture(texturePath);
-                    switch (layer._Type)
-                    {
-                        case LayerType.IntGrid:
-                        case LayerType.AutoLayer:
-                            if (layer.AutoLayerTiles.Length != 0)
-                            {
-                                using (var enumerator = layer.AutoLayerTiles.Where(_ => layer._TilesetDefUid.HasValue).GetEnumerator())
-                                {
-                                    while (enumerator.MoveNext())
-                                    {
-                                        var current = enumerator.Current;
-                                        System.Diagnostics.Debug.Assert(current != null);
-                                        var position = new Vector2((current.Px.X + layer._PxTotalOffsetX) * scale , (current.Px.Y + layer._PxTotalOffsetY) * scale);
-                                        var rectangle = new Rectangle(current.Src.X, current.Src.Y, layer._GridSize, layer._GridSize);
-                                        var f = (SpriteEffects)current.F;
-                                        iBatcher.Draw(texture, position, rectangle, Color.White, 0.0f, Vector2.Zero, scale, f, 0.0f);
-                                    }
-                                    continue;
-                                }
-                            }
-                            else
-                                continue;
-                        case LayerType.Tiles:
-                            using (var enumerator = layer.GridTiles.Where(_ => layer._TilesetDefUid.HasValue).GetEnumerator())
-                            {
-                                while (enumerator.MoveNext())
-                                {
-                                    var current = enumerator.Current;
-                                    System.Diagnostics.Debug.Assert(current != null);
-                                    var position = new Vector2((current.Px.X + layer._PxTotalOffsetX) * scale, (current.Px.Y + layer._PxTotalOffsetY) * scale);
-                                    var rectangle = new Rectangle(current.Src.X, current.Src.Y, layer._GridSize, layer._GridSize);
-                                    var f = (SpriteEffects)current.F;
-                                    iBatcher.Draw(texture, position, rectangle, Color.White, 0.0f, Vector2.Zero, scale, f, 0.0f);
-                                }
-                                continue;
-                            }
-                        default:
-                            continue;
-                    }
-                }
+                var thisLayer = level.LayerInstances[ii];
+                if (thisLayer._TilesetRelPath == null || thisLayer._Type == LayerType.Entities || _layerToTileMap[ii] == null)
+                    continue;
+
+                var thisTileSet = _layerToTileMap[ii];
+
+                IEnumerable<TileInstance> tilesToRender;
+                if (thisLayer._Type == LayerType.AutoLayer || thisLayer._Type == LayerType.IntGrid)
+                    tilesToRender = thisLayer.AutoLayerTiles.Where(_ => thisLayer._TilesetDefUid.HasValue);
+                else
+                    tilesToRender = thisLayer.GridTiles.Where(_ => thisLayer._TilesetDefUid.HasValue);
+
+                RenderTiles(thisLayer, tilesToRender, thisTileSet, iBatcher, scale);
             }
         }
 
@@ -82,6 +57,28 @@ namespace Raspberry_Lib.Renderers
         private readonly string _path;
         private readonly ContentManager _contentManager;
         private readonly LDtkWorld _world;
+        private readonly int _level;
+        private Dictionary<int, Texture2D> _layerToTileMap;
+
+        Dictionary<int, Texture2D> PreloadLevelTilesets()
+        {
+            var dictionaryToReturn = new Dictionary<int, Texture2D>();
+
+            var layers = _world.Levels[_level].LayerInstances;
+            for (var ii = 0; ii < layers.Length; ii++)
+            {
+                var thisLayer = layers[ii];
+
+                if (string.IsNullOrWhiteSpace(thisLayer._TilesetRelPath))
+                    continue;
+
+                var texturePath = Path.Combine(GetParentPath(_path), thisLayer._TilesetRelPath);
+                var texture = this.LoadTexture(texturePath);
+                dictionaryToReturn.Add(ii, texture);
+            }
+
+            return dictionaryToReturn;
+        }
 
         private Texture2D LoadTexture(string iPath) => _contentManager.Load<Texture2D>(Path.ChangeExtension(iPath, null));
 
@@ -97,6 +94,18 @@ namespace Raspberry_Lib.Renderers
             }
 
             return iPath;
+        }
+
+        private void RenderTiles(LayerInstance iLayer, IEnumerable<TileInstance> iTiles, Texture2D iTileSet, Batcher iBatcher, float iScale)
+        {
+            foreach (var tileInstance in iTiles)
+            {
+                System.Diagnostics.Debug.Assert(tileInstance != null);
+                var position = new Vector2((tileInstance.Px.X + iLayer._PxTotalOffsetX) * iScale, (tileInstance.Px.Y + iLayer._PxTotalOffsetY) * iScale);
+                var rectangle = new Rectangle(tileInstance.Src.X, tileInstance.Src.Y, iLayer._GridSize, iLayer._GridSize);
+                var f = (SpriteEffects)tileInstance.F;
+                iBatcher.Draw(iTileSet, position, rectangle, Color.White, 0.0f, Vector2.Zero, iScale, f, 0.0f);
+            }
         }
     }
 }
