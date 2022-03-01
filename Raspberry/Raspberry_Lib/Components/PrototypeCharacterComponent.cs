c2d3fb68e494fbe782c54483f1ab666595ca7d0a
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-03-01 16:26:59 -0700

    Adding in simple left and right movement animations

--- Raspberry/Raspberry_Lib/Components/PrototypeCharacterComponent.cs
+++ Raspberry/Raspberry_Lib/Components/PrototypeCharacterComponent.cs
@@ -1,28 +1,103 @@
-﻿using Nez;
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using Nez;
 using Nez.Sprites;
 using Nez.Textures;
 
 namespace Raspberry_Lib.Components
 {
-    internal class PrototypeCharacterComponent : Component//, IUpdatable
+    internal class PrototypeCharacterComponent : Component
+    {
+        public enum State
+        {
+            Idle,
+            RunLeft,
+            RunRight
+        }
+
+        public override void OnAddedToEntity()
+        {
+            _animationComponent = Entity.AddComponent(new CharacterAnimationComponent());
+            Entity.AddComponent(new CharacterInputController(OnCharacterStateChanged));
+        }
+
+        private CharacterAnimationComponent _animationComponent;
+
+        private void OnCharacterStateChanged(State iNewState)
+        {
+            _animationComponent.SetState(iNewState);
+        }
+    }
+
+    internal class CharacterAnimationComponent : Component
     {
         public override void OnAddedToEntity()
         {
             var texture = Entity.Scene.Content.LoadTexture(@"Characters/PrototypeCharacter");
-            var sprites = Sprite.SpritesFromAtlas(texture, 16, 24);
+            var sprites = Sprite.SpritesFromAtlas(texture, 24, 24);
 
-            //_mover = Entity.AddComponent(new Mover())
             _animator = Entity.AddComponent<SpriteAnimator>();
 
-            _animator.AddAnimation("Idle", new[] { sprites[0], sprites[1], sprites[2], sprites[3] });
-            _animator.Play("Idle", SpriteAnimator.LoopMode.PingPong);
+            _animator.AddAnimation(PrototypeCharacterComponent.State.Idle.ToString(), new[] { sprites[0], sprites[1], sprites[2], sprites[3] });
+            _animator.AddAnimation(PrototypeCharacterComponent.State.RunLeft.ToString(), new []{sprites[5]});
+            _animator.AddAnimation(PrototypeCharacterComponent.State.RunRight.ToString(), new []{sprites[4]});
+
+            _currentState = PrototypeCharacterComponent.State.Idle;
+            _animator.Play(PrototypeCharacterComponent.State.Idle.ToString(), SpriteAnimator.LoopMode.PingPong);
         }
 
-        // public void Update()
-        // {
-        //
-        // }
+        public void SetState(PrototypeCharacterComponent.State iNewState)
+        {
+            if (_currentState == iNewState)
+                return;
 
+            _currentState = iNewState;
+            switch (iNewState)
+            {
+                case PrototypeCharacterComponent.State.Idle:
+                    _animator.Play(PrototypeCharacterComponent.State.Idle.ToString(), SpriteAnimator.LoopMode.PingPong);
+                    break;
+                case PrototypeCharacterComponent.State.RunLeft:
+                    _animator.Play(PrototypeCharacterComponent.State.RunLeft.ToString(), SpriteAnimator.LoopMode.ClampForever);
+                    break;
+                case PrototypeCharacterComponent.State.RunRight:
+                    _animator.Play(PrototypeCharacterComponent.State.RunRight.ToString(), SpriteAnimator.LoopMode.ClampForever);
+                    break;
+                default:
+                    System.Diagnostics.Debug.Fail($"Unknown value of enum {typeof(PrototypeCharacterComponent.State)}: {iNewState}");
+                    throw new ArgumentOutOfRangeException(nameof(iNewState), iNewState, null);
+            }
+        }
+
+        private PrototypeCharacterComponent.State _currentState;
         private SpriteAnimator _animator;
+    }
+
+    internal class CharacterInputController: Component, IUpdatable
+    {
+        public CharacterInputController(Action<PrototypeCharacterComponent.State> iOnStateChangedCallback)
+        {
+            _onStateChangedCallback = iOnStateChangedCallback;
+        }
+
+        public override void OnAddedToEntity()
+        {
+            _xAxisInput = new VirtualIntegerAxis(new VirtualAxis.KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.Left, Keys.Right));
+        }
+
+        public void Update()
+        {
+            var inputX = _xAxisInput.Value;
+
+            if (inputX == 0)
+                _onStateChangedCallback(PrototypeCharacterComponent.State.Idle);
+            else if (inputX > 0)
+                _onStateChangedCallback(PrototypeCharacterComponent.State.RunRight);
+            else
+                _onStateChangedCallback(PrototypeCharacterComponent.State.RunLeft);
+        }
+
+        private VirtualIntegerAxis _xAxisInput;
+        private readonly Action<PrototypeCharacterComponent.State> _onStateChangedCallback;
     }
 }
