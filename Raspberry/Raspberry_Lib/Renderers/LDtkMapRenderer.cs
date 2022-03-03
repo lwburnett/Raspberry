d3b8dc87bca59b8f5f26b5e389fe784bb40533aa
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-03-03 16:24:15 -0700

    Creating colliders on relevant tiles

--- Raspberry/Raspberry_Lib/Renderers/LDtkMapRenderer.cs
+++ Raspberry/Raspberry_Lib/Renderers/LDtkMapRenderer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using LDtk;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -25,12 +26,13 @@ namespace Raspberry_Lib.Renderers
             _layersToTileMap = PreloadLevelTilesets();
         }
 
+        public int PhysicsLayer = 1 << 0;
         public override float Width => _world.Levels[_level].PxWid;
         public override float Height => _world.Levels[_level].PxHei;
         
         public override void Render(Batcher iBatcher, Camera iCamera)
         {
-            var level = _world.Levels[0];
+            var level = _world.Levels[_level];
             System.Diagnostics.Debug.Assert(Math.Abs(Entity.Transform.Scale.X - Entity.Transform.Scale.Y) < .001);
             var scale = Entity.Transform.Scale.X;
             for (var ii = level.LayerInstances.Length - 1; ii >= 0; --ii)
@@ -51,13 +53,36 @@ namespace Raspberry_Lib.Renderers
             }
         }
 
+        public override void OnAddedToEntity() => AddColliders();
+
+        public override void OnRemovedFromEntity() => RemoveColliders();
+
+        public override void OnEntityTransformChanged(Transform.Component comp)
+        {
+            // This was directly copied from TiledMapRenderer.cs. No idea if LDtk maps can be scaled
+            // we only deal with positional changes here. TiledMaps cant be scaled.
+            if (comp == Transform.Component.Position)
+            {
+                RemoveColliders();
+                AddColliders();
+            }
+        }
+
         public LDtkWorld World => _world;
+
+        private enum CollisionEnum
+        {
+            //Nothing,
+            //Overlappable,
+            Collidable
+        }
 
         private readonly string _path;
         private readonly ContentManager _contentManager;
         private readonly LDtkWorld _world;
         private readonly int _level;
         private readonly Dictionary<int, Texture2D> _layersToTileMap;
+        private List<Collider> _colliders;
 
         Dictionary<int, Texture2D> PreloadLevelTilesets()
         {
@@ -105,6 +130,66 @@ namespace Raspberry_Lib.Renderers
                 var f = (SpriteEffects)tileInstance.F;
                 iBatcher.Draw(iTileSet, position, rectangle, Color.White, 0.0f, Vector2.Zero, iScale, f, 0.0f);
             }
+        }
+
+        private void AddColliders()
+        {
+            System.Diagnostics.Debug.Assert(_world != null);
+            System.Diagnostics.Debug.Assert(Math.Abs(Entity.Transform.Scale.X - Entity.Transform.Scale.Y) < .001);
+
+            _colliders = new List<Collider>();
+            var level = _world.Levels[_level];
+            var scale = Entity.Transform.Scale.X;
+            
+            for (var ii = level.LayerInstances.Length - 1; ii >= 0; --ii)
+            {
+                var thisLayer = level.LayerInstances[ii];
+                if (!thisLayer._TilesetDefUid.HasValue)
+                    continue;
+
+                var tileSet = _world.Defs.Tilesets.FirstOrDefault(iT => iT.Uid == thisLayer._TilesetDefUid);
+                System.Diagnostics.Debug.Assert(tileSet != null);
+                var collidableTag = tileSet.EnumTags.FirstOrDefault(iEt => ((JsonElement)iEt["enumValueId"]).GetString() == CollisionEnum.Collidable.ToString());
+                System.Diagnostics.Debug.Assert(collidableTag != null);
+
+                var collidableIdArray = new List<int>(); 
+                var enumerator = ((JsonElement)collidableTag["tileIds"]).EnumerateArray();
+                while (enumerator.MoveNext())
+                {
+                    var current = enumerator.Current.ToString();
+                    var success = int.TryParse(current, out var result);
+                    System.Diagnostics.Debug.Assert(success);
+                    collidableIdArray.Add(result);
+                }
+
+                List<TileInstance> tilesToCheck = new List<TileInstance>();
+                tilesToCheck.AddRange(thisLayer.AutoLayerTiles);
+                tilesToCheck.AddRange(thisLayer.GridTiles);
+                foreach (var thisTile in tilesToCheck)
+                {
+                    if (collidableIdArray.Contains(thisTile.T))
+                    {
+                        var rectangle = new Rectangle((int)(thisTile.Src.X * scale), (int)(thisTile.Src.Y * scale), (int)(thisLayer._GridSize * scale), (int)(thisLayer._GridSize * scale));
+                        var collider = new BoxCollider(rectangle)
+                        {
+                            PhysicsLayer = PhysicsLayer,
+                            Entity = Entity
+                        };
+                        _colliders.Add(collider);
+                        Physics.AddCollider(collider);
+                    }
+                }
+            }
+        }
+
+        private void RemoveColliders()
+        {
+            if (_colliders == null)
+                return;
+
+            foreach (var collider in _colliders)
+                Physics.RemoveCollider(collider);
+            _colliders.Clear();
         }
     }
 }
