3c835688f4129999aee788b48b275a450fccb7f5
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-06-29 23:49:37 -0600

    Updating the android build version

--- Raspberry/Raspberry_Android/MainActivity.cs
+++ Raspberry/Raspberry_Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Views;
 using Microsoft.Xna.Framework;
 using Raspberry_Lib;
+using Xamarin.Essentials;
 
 namespace Raspberry_Android
 {
@@ -11,9 +12,10 @@ namespace Raspberry_Android
         Label = "@string/app_name",
         MainLauncher = true,
         Icon = "@drawable/icon",
+        Theme="@android:style/Theme.NoTitleBar.Fullscreen",
         AlwaysRetainTaskState = true,
         LaunchMode = LaunchMode.SingleInstance,
-        ScreenOrientation = ScreenOrientation.FullUser,
+        ScreenOrientation = ScreenOrientation.SensorLandscape,
         ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize
     )]
     public class MainActivity : AndroidGameActivity
@@ -21,9 +23,13 @@ namespace Raspberry_Android
         private GameMaster _game;
         private View _view;
 
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle iBundle)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(iBundle);
+
+            Platform.Init(this, iBundle);
+
+            Window?.AddFlags(WindowManagerFlags.Fullscreen);
 
             _game = new GameMaster(true);
             _view = _game.Service.GetService(typeof(View)) as View;
