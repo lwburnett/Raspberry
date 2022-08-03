53898399cfcc1baea697c819411ac92bec9d4b28
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-08-03 09:23:47 -0600

    Moving the ProximityMaterial and ProximityEffect into its own file

--- Raspberry/Raspberry_Lib/Components/ProximitySpriteRenderer.cs
+++ Raspberry/Raspberry_Lib/Components/ProximitySpriteRenderer.cs
@@ -69,45 +69,4 @@ namespace Raspberry_Lib.Components
 
         private ProximityMaterial _material;
     }
-
-    internal class ProximityMaterial : Material<ProximityEffect>
-    {
-        public ProximityMaterial(Texture2D iInsideTexture, Texture2D iOutsideTexture, Vector2 iPositionTopLeft, Vector2 iSpriteDimensions, Vector2 iScreenDimension)
-        {
-            Effect = new ProximityEffect(iInsideTexture, iOutsideTexture, iPositionTopLeft, iSpriteDimensions, iScreenDimension);
-        }
-    }
-
-    internal class ProximityEffect : Effect
-    {
-        public ProximityEffect(Texture2D iInsideTexture, Texture2D iOutsideTexture, Vector2 iPositionTopLeft, Vector2 iSpriteDimensions, Vector2 iScreenDimensions) : 
-            base(Core.GraphicsDevice, EffectResource.GetFileResourceBytes(Content.ContentData.AssetPaths.ProximityShader))
-        {
-            Parameters["InsideTexture"].SetValue(iInsideTexture);
-            
-            Parameters["OutsideTexture"].SetValue(iOutsideTexture);
-
-            Parameters["SpritePositionTopLeft"].SetValue(iPositionTopLeft);
-
-            Parameters["SpriteDimensions"].SetValue(iSpriteDimensions);
-
-            Parameters["ScreenDimensions"].SetValue(iScreenDimensions);
-
-            _playerPositionParam = Parameters["PlayerPosition"];
-            _proximityRadiusParam = Parameters["ProximityRadius"];
-        }
-
-        public void SetPlayerPosition(Vector2 iPlayerPosition)
-        {
-            _playerPositionParam.SetValue(iPlayerPosition);
-        }
-
-        public void SetProximityRadius(float iProximityRadius)
-        {
-            _proximityRadiusParam.SetValue(iProximityRadius);
-        }
-        
-        private readonly EffectParameter _playerPositionParam;
-        private readonly EffectParameter _proximityRadiusParam;
-    }
 }
