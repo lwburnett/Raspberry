31bb49340e062041097dfc580fbbf868e95d9149
DESKTOP-S3R1GSR\Luke <lwburnett33@gmail.com>
2022-11-08 22:02:04 -0700

    Adding in the next stage of the tutorial about energy decay

--- Raspberry/Raspberry_Lib/Components/TutorialUiCanvasComponent.cs
+++ Raspberry/Raspberry_Lib/Components/TutorialUiCanvasComponent.cs
@@ -15,7 +15,10 @@ namespace Raspberry_Lib.Components
             public static readonly RenderSetting BackgroundTextureWidth = new(1500);
             public static readonly RenderSetting BackgroundTextureHeight = new(200);
             public static readonly Color TextBoxBackgroundTextureColor = new(112, 128, 144, 200);
-            public static readonly Color DistanceDisplayBackgroundTextureColor = new(136, 8, 8, 200);
+            public static readonly Color HighlightDisplayBackgroundTextureColor = new(136, 8, 8, 200);
+            public static readonly RenderSetting EnergyDisplayThickness = new(5);
+            public const float EnergyDecayPercentLossPerSecond = .15f;
+            public const float EnergyDecayMinimumScale = .33f;
 
             public const float BlinkPeriodSeconds = 1f;
         }
@@ -28,6 +31,8 @@ namespace Raspberry_Lib.Components
             StoryIntro3,
             GameGoal1,
             DistanceDisplay,
+            EnergyDisplay,
+            EnergyDecay,
             EndPlay
         }
 
@@ -35,6 +40,8 @@ namespace Raspberry_Lib.Components
         {
             _currentState = 0;
             _timeSinceLastBlinkToggle = null;
+            _blinkToggle = false;
+            _energyDisplaySizeMultiplier = 1f;
         }
 
         private State _currentState;
@@ -48,7 +55,10 @@ namespace Raspberry_Lib.Components
         private float? _timeSinceLastBlinkToggle;
         private bool _blinkToggle;
 
+        private float _energyDisplaySizeMultiplier;
+
         private Nez.UI.IDrawable _distanceDisplayBackground;
+        private Image _energyDisplay;
 
         protected sealed override void OnAddedToEntityInternal()
         {
@@ -86,6 +96,11 @@ namespace Raspberry_Lib.Components
             _characterInputController = characterEntity.GetComponent<CharacterInputController>();
 
             System.Diagnostics.Debug.Assert(_characterProximity != null);
+            System.Diagnostics.Debug.Assert(_characterInputController != null);
+
+            var energyDisplayTexture = CreateEnergyDisplayTexture(_characterProximity.Radius);
+            _energyDisplay = Canvas.Stage.AddElement(new Image(energyDisplayTexture));
+            _energyDisplay.SetIsVisible(false);
 
             OnNavigationChanged(State.Welcome, State.Welcome);
         }
@@ -94,20 +109,50 @@ namespace Raspberry_Lib.Components
         {
             base.UpdateInternal();
 
-            if (!_timeSinceLastBlinkToggle.HasValue)
-                return;
-
-            _timeSinceLastBlinkToggle += Time.DeltaTime;
-
-            if (_currentState == State.DistanceDisplay)
+            if (_timeSinceLastBlinkToggle.HasValue)
             {
-                if (_timeSinceLastBlinkToggle.Value >= Settings.BlinkPeriodSeconds)
+                _timeSinceLastBlinkToggle += Time.DeltaTime;
+
+                if (_currentState == State.DistanceDisplay)
                 {
-                    _blinkToggle = !_blinkToggle;
-                    _timeSinceLastBlinkToggle = 0;
-                    var drawable = _blinkToggle ? _distanceDisplayBackground : null;
-                    DistanceLabel.SetBackground(drawable);
+                    if (_timeSinceLastBlinkToggle.Value >= Settings.BlinkPeriodSeconds)
+                    {
+                        _blinkToggle = !_blinkToggle;
+                        _timeSinceLastBlinkToggle = 0;
+                        var drawable = _blinkToggle ? _distanceDisplayBackground : null;
+                        DistanceLabel.SetBackground(drawable);
+                    }
                 }
+                else if (_currentState == State.EnergyDisplay)
+                {
+                    var characterPositionInScreenSpace =
+                        Entity.Scene.Camera.WorldToScreenPoint(_characterProximity.Entity.Position);
+
+                    _energyDisplay.SetPosition(
+                        characterPositionInScreenSpace.X - _characterProximity.Radius,
+                        characterPositionInScreenSpace.Y - _characterProximity.Radius);
+
+                    if (_timeSinceLastBlinkToggle.Value >= Settings.BlinkPeriodSeconds)
+                    {
+                        _blinkToggle = !_blinkToggle;
+                        _timeSinceLastBlinkToggle = 0;
+                        _energyDisplay.SetIsVisible(_blinkToggle);
+                    }
+                }
+            }
+            
+            if (_currentState == State.EnergyDecay)
+            {
+                var currentScale = _energyDisplay.GetWidth() / (_characterProximity.Radius * 2); 
+                var potentialNewScale = currentScale - Settings.EnergyDecayPercentLossPerSecond * Time.DeltaTime;
+                _energyDisplaySizeMultiplier = potentialNewScale >= Settings.EnergyDecayMinimumScale ? potentialNewScale : 1f;
+                
+                var boatPosition = Entity.Scene.Camera.WorldToScreenPoint(_characterProximity.Entity.Position);
+                _energyDisplay.SetBounds(
+                    boatPosition.X - _characterProximity.Radius * _energyDisplaySizeMultiplier,
+                    boatPosition.Y - _characterProximity.Radius * _energyDisplaySizeMultiplier,
+                    2 * _characterProximity.Radius * _energyDisplaySizeMultiplier,
+                    2 * _characterProximity.Radius * _energyDisplaySizeMultiplier);
             }
         }
 
@@ -123,7 +168,7 @@ namespace Raspberry_Lib.Components
         {
             var width = (int)DistanceLabel.PreferredWidth;
             var height = (int)DistanceLabel.PreferredHeight;
-            return CreateSolidColorTexture(width, height, Settings.DistanceDisplayBackgroundTextureColor);
+            return CreateSolidColorTexture(width, height, Settings.HighlightDisplayBackgroundTextureColor);
         }
 
         private Texture2D CreateSolidColorTexture(int iWidth, int iHeight, Color iColor)
@@ -134,6 +179,35 @@ namespace Raspberry_Lib.Components
                 textureData[ii] = iColor;
             }
             var texture = new Texture2D(Graphics.Instance.Batcher.GraphicsDevice, iWidth, iHeight);
+            texture.SetData(textureData);
+
+            return texture;
+        }
+
+        private Texture2D CreateEnergyDisplayTexture(float iRadius)
+        {
+            var radiusInt = (int)iRadius;
+
+            var diameter = 2 * radiusInt;
+
+            var center = new Vector2(radiusInt);
+            var textureData = new Color[diameter * diameter];
+            for (var ii = 0; ii < diameter; ii++)
+            for (var jj = 0; jj < diameter; jj++)
+            {
+                var thisCoordinate = new Vector2(ii, jj);
+
+                var distance = Math.Abs((center - thisCoordinate).Length());
+
+                var diff = iRadius - distance;
+
+                if (0 < diff && diff < Settings.EnergyDisplayThickness.Value)
+                    textureData[jj * diameter + ii] = Settings.HighlightDisplayBackgroundTextureColor;
+                else
+                    textureData[jj * diameter + ii] = Color.Transparent;
+            }
+
+            var texture = new Texture2D(Graphics.Instance.Batcher.GraphicsDevice, diameter, diameter);
             texture.SetData(textureData);
 
             return texture;
@@ -152,6 +226,16 @@ namespace Raspberry_Lib.Components
                 case State.DistanceDisplay:
                     DistanceLabel.SetBackground(null);
                     _timeSinceLastBlinkToggle = null;
+                    break;
+                case State.EnergyDisplay:
+                    _energyDisplay.SetIsVisible(false);
+                    _timeSinceLastBlinkToggle = null;
+                    break;
+                case State.EnergyDecay:
+                    _energyDisplay.SetIsVisible(false);
+                    var radius = _characterProximity.Radius;
+                    var boatPosition = Entity.Scene.Camera.WorldToScreenPoint(_characterProximity.Entity.Position);
+                    _energyDisplay.SetBounds(boatPosition.X - radius, boatPosition.Y - radius, radius * 2, radius * 2);
                     break;
                 case State.Welcome:
                 case State.StoryIntro1:
@@ -185,6 +269,12 @@ namespace Raspberry_Lib.Components
                 case State.DistanceDisplay:
                     HandleDistanceDisplay();
                     break;
+                case State.EnergyDisplay:
+                    HandleEnergyDisplay();
+                    break;
+                case State.EnergyDecay:
+                    HandleEnergyDecay();
+                    break;
                 case State.EndPlay:
                     HandleEndPlay();
                     break;
@@ -215,6 +305,22 @@ namespace Raspberry_Lib.Components
             _timeSinceLastBlinkToggle = 0;
             DistanceLabel.SetBackground(_distanceDisplayBackground);
             HandleGenericTextChange("Distance is shown at the top of the screen.");
+        }
+
+        private void HandleEnergyDisplay()
+        {
+            _blinkToggle = false;
+            _timeSinceLastBlinkToggle = .5f;
+            _energyDisplay.SetIsVisible(false);
+            HandleGenericTextChange("Energy level is shown by the size of the\nbubble around the boat.");
+        }
+
+        private void HandleEnergyDecay()
+        {
+            _timeSinceLastBlinkToggle = null;
+            _energyDisplay.SetIsVisible(true);
+            _energyDisplaySizeMultiplier = 1f;
+            HandleGenericTextChange("Over time your energy level will decay,\nmaking the bubble smaller. If the bubble\nencloses around the boat, you lose.");
         }
 
         private void HandleEndPlay()
